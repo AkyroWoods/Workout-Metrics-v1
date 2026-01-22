5927efbc06396e960b40a13f3e0dc89454dfe153
Akyro Woods <akyrowoods07@gmail.com>
2026-01-22 12:35:51 -0700

    Add muscle group field and implement Push/Pull/Legs classification

--- src/Exercise.java
+++ src/Exercise.java
@@ -3,11 +3,13 @@ public class Exercise {
     private int sets;
     private int reps;
     private double weight;
+    private String muscleGroup;
 
-    public Exercise(String name, int sets, int reps, double weight)  throws IllegalArgumentException{
+    public Exercise(String name, int sets, int reps, double weight, String muscleGroup)
+            throws IllegalArgumentException {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Exercise name cannot be blank");
-        } 
+        }
         if (sets < 1) {
             throw new IllegalArgumentException("Sets cannot be less than 1");
         }
@@ -17,10 +19,14 @@ public class Exercise {
         if (weight < 0) {
             throw new IllegalArgumentException("Weight cannot be negative");
         }
+        if (muscleGroup == null || muscleGroup.isBlank()) {
+            throw new IllegalArgumentException("Muscle group cannot be blank");
+        }
         this.name = name;
         this.sets = sets;
         this.reps = reps;
         this.weight = weight;
+        this.muscleGroup = muscleGroup;
     }
 
     public String getName() {
@@ -38,14 +44,33 @@ public class Exercise {
     public double getWeight() {
         return weight;
     }
-    
+
+    public String getMuscleGroup() {
+        return muscleGroup;
+    }
+
     public double calculateTotalVolume() {
         return weight * (sets * reps);
+    }
+
+    public String classifyExercise() {
+        String muscleGroup = this.muscleGroup.toLowerCase();
+        if (muscleGroup.equals("chest") || muscleGroup.equals("shoulders")
+                || muscleGroup.equals("triceps")) {
+            return "Push";
+        } else if (muscleGroup.equals("lats") || muscleGroup.equals("biceps")
+                || muscleGroup.contains("upper back") || muscleGroup.equals("erectors")) {
+                    return "Pull";
+                } else if (muscleGroup.equals("quads") || muscleGroup.equals("hamstrings")
+                || muscleGroup.equals("calves")) {
+                    return "Legs";
+                }
+                return "Other";
     }
 
     @Override
     public String toString() {
         return name + " -  Sets: " + sets + ", Reps: " + reps + ", Weight: " + weight;
     }
-    
+
 }
